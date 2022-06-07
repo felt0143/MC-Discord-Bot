47773c13f13ca34540a04778ec7ed09c24b65b38
felt0143 <felt0143@umn.edu>
2022-06-06 20:40:39 -0500

    Removing annoying double quotes.

--- bot.py
+++ bot.py
@@ -15,12 +15,12 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix="$")
+bot = commands.Bot(command_prefix='$')
 
 
 @bot.command()
 async def ping(context):
-    await context.channel.send("pong")
+    await context.channel.send('pong')
 
 
 @bot.command(pass_context=True)
